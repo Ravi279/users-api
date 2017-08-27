6ad602f59b5866c2195d0bfc87b931ae46029cb5
Raviteja Pokala <ravi.ror@aol.com>
2017-08-27 00:47:58 -0400

    implemented has secure token for key

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,18 +14,18 @@ class Api::V1::UsersController < ApplicationController
 
 	# POST /v1/users
 	def create
-		respond_with :api, :v1, User.new(user_params)
+		@user = User.new(user_params)
 
 		if @user.save
-		  render json: @user, status: :created, location: @user
+			respond_with @user, status: '201 Created', location: nil
 		else
-		  render json: @user.errors, status: :unprocessable_entity
+			respond_with @user.errors, status: '422 Unprocessable Entity'
 		end
 	end
 
   private
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email, :phone_number, :full_name, :password, :metadata)
+		params.require(:user).permit(:email, :phone_number, :full_name, :password_digest, :metadata)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,27 @@
 class User < ApplicationRecord
+	# before_validation :generate_user_key, on: :create
 
 	has_secure_password
+	has_secure_token :key
+
+	validates :email, presence: true, uniqueness: true, length: {maximum: 200}
+	validates :phone_number, presence: true, uniqueness: true, length: {maximum: 20}
+	validates :full_name, length: {maximum: 200}
+	validates :key, uniqueness: true, length: {maximum: 200}
+	validates :account_key, uniqueness: true, length: {maximum: 100}
+	validates :metadata, length: {maximum: 2000}
+
 
 	def self.search(query)
 		where("email LIKE :search OR full_name LIKE :search OR metadata LIKE :search", search: "%#{query}%")
 	end
 
-	private
-
+	# Generate and assign unique key
 	def generate_user_key
-		begin
-			random_hash = SecureRandom.hex(13)
-		end while User.exists?(api_key: random_hash)
+		self.key = loop do
+            random_hex = SecureRandom.urlsafe_base64
+            break random_hex unless self.class.exists?(key: random_hex)
+        end
 	end
 
 end
